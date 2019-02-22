2f4fa7f56cef49946142310afc4004903685666f
TGSully <36617773+TGSully@users.noreply.github.com>
2019-02-22 12:26:47 -0500

    Add files via upload

---
+++ outletAnimate1/Assets/onMouseOver.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onMouseOver : MonoBehaviour {
+    //Get the GameObject’s mesh renderer to access the GameObject’s material and color
+    MeshRenderer m_Renderer;
+
+    public delegate void PlayAction();
+    public static event PlayAction OnPlay;
+
+
+    Material white;
+    Material grey;
+    Material vid;
+
+    void Start()
+    {
+        //Fetch the mesh renderer component from the GameObject
+        m_Renderer = GetComponent<MeshRenderer>();
+        //Fetch the original color of the GameObject
+        //m_OriginalColor = m_Renderer.material.color;
+        white = Resources.Load("white", typeof(Material)) as Material;
+        grey = Resources.Load("grey", typeof(Material)) as Material;
+        vid = Resources.Load("bluedop", typeof(Material)) as Material;
+        m_Renderer.material = grey;
+
+    }
+
+    void OnMouseOver()
+    {
+        // Change the color of the GameObject to red when the mouse is over GameObject
+        //m_Renderer.material = white;
+        if (OnPlay != null)
+        {
+            OnPlay();
+        }
+
+        //print("emitting farts");
+
+    }
+
+    //void OnMouseExit()
+    //{
+    //    // Reset the color of the GameObject back to normal
+    //    m_Renderer.material = grey;
+    //}
+
+}
---
+++ outletAnimate1/Assets/plane.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class plane : MonoBehaviour
+{
+    Material white;
+    Material grey;
+    VideoClip animate;
+    VideoPlayer videoPlayer;
+    GameObject vidPlane;
+
+    void OnEnable()
+    {
+        onMouseOver.OnPlay += Play;
+    }
+
+    void Play()
+    {
+        videoPlayer.Play();
+    }
+
+
+    void Start()
+    {
+        white = Resources.Load("white", typeof(Material)) as Material;
+        grey = Resources.Load("animate", typeof(Material)) as Material;
+
+        vidPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        Renderer planeRend = vidPlane.GetComponent<Renderer>();
+        planeRend.material = white;
+        videoPlayer = vidPlane.AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.playbackSpeed = 2.5f;
+        //videoPlayer = vidPlane.GetComponent<VideoPlayer>();
+        animate = Resources.Load("bluedop", typeof(VideoClip)) as VideoClip;
+        videoPlayer.clip = animate;
+
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, -0.4f, 0);
+        Renderer cubeRend = cube.GetComponent<Renderer>();
+        cubeRend.material = grey;
+        cube.AddComponent<onMouseOver>();
+
+
+        //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //sphere.transform.position = new Vector3(0, 1.5f, 0);
+
+        //GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //capsule.transform.position = new Vector3(2, 1, 0);
+
+        //GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        //cylinder.transform.position = new Vector3(-2, 1, 0);
+    }
+}
