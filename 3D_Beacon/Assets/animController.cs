c03431d9f4150503b365c9006bee29624753ddfc
TGSully <36617773+TGSully@users.noreply.github.com>
2019-03-14 16:05:33 -0400

    Add files via upload

---
+++ 3D_Beacon/Assets/animController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		//anim["beaconAnimation"].wrapMode = WrapMode.Once;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+		{
+			anim.Play("beaconAnimation");
+			//anim.StopPlayback("beaconAnimation");
+		}
+	}
+}
---
+++ 3D_Beacon/Assets/createOutlet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createOutlet : MonoBehaviour 
+{
+	Material planeColor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		planeColor = Resources.Load("planeColor", typeof(Material)) as Material;
+
+		GameObject plane  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0,0);
+		cube.transform.localScale -= new Vector3(0.7f, 0.9f, 0.8f);
+		Renderer cubeRend = cube.GetComponent<Renderer>();
+		cubeRend.material = planeColor;
+	}
+}
